dea0fa72d850700e2b81215cc85f5d29d34a4e12
wenchao.ren <wenchao.ren@bianlifeng.com>
2019-08-13 20:06:18 +0800

    1. 完善proxy ui的文档 2. 优化了默认的agent的启动脚本中agent store的默认目录，将agent的store默认目录放置在和lib，bin同级的目录下 3. 优化了默认的ui启动脚本中，cache的默认目录。将默认目录修改为和lib，bin同级的目录中。

--- bistoury-dist/bin/base.sh
+++ bistoury-dist/bin/base.sh
@@ -6,6 +6,7 @@ BISTOURY_COF_DIR="$BISTOURY_BIN_DIR/../conf"
 BISTOURY_PID_DIR="$BISTOURY_BIN_DIR/../pid"
 BISTOURY_LOG_DIR="$BISTOURY_BIN_DIR/../logs"
 BISTOURY_LIB_DIR="$BISTOURY_BIN_DIR/../lib"
+BISTOURY_STORE_DIR="$BISTOURY_BIN_DIR/../store"
 
 if [[ ! -w "$BISTOURY_PID_DIR" ]] ; then
 mkdir -p "$BISTOURY_PID_DIR"
@@ -13,6 +14,10 @@ fi
 
 if [[ ! -w "$BISTOURY_LOG_DIR" ]] ; then
 mkdir -p "$BISTOURY_LOG_DIR"
+fi
+
+if [[ ! -w "$BISTOURY_STORE_DIR" ]] ; then
+mkdir -p "$BISTOURY_STORE_DIR"
 fi
 
 CLASSPATH="$BISTOURY_COF_DIR"
