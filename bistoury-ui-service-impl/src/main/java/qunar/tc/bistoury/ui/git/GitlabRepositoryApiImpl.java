3662f35562e1efc3846c80567f1b340481b64a06
leix.xie <leix.xie@gmail.com>
2019-09-09 14:16:28 +0800

    文档修改

--- bistoury-ui-service-impl/src/main/java/qunar/tc/bistoury/ui/git/GitlabRepositoryApiImpl.java
+++ bistoury-ui-service-impl/src/main/java/qunar/tc/bistoury/ui/git/GitlabRepositoryApiImpl.java
@@ -52,7 +52,7 @@ public class GitlabRepositoryApiImpl implements GitRepositoryApi {
     public GitlabRepositoryApiImpl(GitPrivateTokenService privateTokenService, DynamicConfig config) {
         this.gitPrivateTokenService = privateTokenService;
         filePathFormat = config.getString("file.path.format", "{0}src/main/java/{1}.java");
-        gitEndPoint = config.getString("gitlab.endpoint");
+        gitEndPoint = config.getString("git.endpoint");
     }
 
     @Override
