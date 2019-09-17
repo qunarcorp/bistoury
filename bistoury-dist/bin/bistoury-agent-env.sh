d8081dd0c2329e5bc1a136a5bd7e8c86dcac8105
cai.wen <cai.wen@qunar.com>
2019-09-17 11:33:35 +0800

    qj相关的命令java_home有问题

--- bistoury-dist/bin/bistoury-agent-env.sh
+++ bistoury-dist/bin/bistoury-agent-env.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 BISTOURY_STORY_PATH="$BISTOURY_STORE_DIR"
 BISTOURY_PROXY_HOST="127.0.0.1:9090"
 BISTOURY_APP_LIB_CLASS="org.springframework.web.servlet.DispatcherServlet"
-JAVA_HOME="/tmp/bistoury/java"
+if [[ -z $JAVA_HOME ]];then
+         JAVA_HOME="/tmp/bistoury/java"
+fi
 JAVA_OPTS="-Dbistoury.store.path=$BISTOURY_STORY_PATH -Dbistoury.proxy.host=$BISTOURY_PROXY_HOST"
