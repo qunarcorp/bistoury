c7ba2eb8f115aff325408b3c7ce67bdf27084980
cai.wen <cai.wen@qunar.com>
2019-09-19 19:23:25 +0800

    移除解析解析agentid的开关

--- script/docker/remote_repositories_start.sh
+++ script/docker/remote_repositories_start.sh
@@ -11,7 +11,7 @@ docker run -d -p 2181:2181 -it --net bistoury --ip 172.19.0.2 registry.cn-hangzh
 sleep 30
 #proxy 镜像
 echo "start run proxy module"
-docker run -d -p 9880:9880 -p 9881:9881 -p 9090:9090 -i --net bistoury --ip 172.19.0.3 registry.cn-hangzhou.aliyuncs.com/bistoury/bistoury-proxy --real-ip $1 --zk-address 172.19.0.2:2181 --parse-agent-id true --proxy-jdbc-url jdbc:mysql://172.19.0.7:3306/bistoury
+docker run -d -p 9880:9880 -p 9881:9881 -p 9090:9090 -i --net bistoury --ip 172.19.0.3 registry.cn-hangzhou.aliyuncs.com/bistoury/bistoury-proxy --real-ip $1 --zk-address 172.19.0.2:2181 --proxy-jdbc-url jdbc:mysql://172.19.0.7:3306/bistoury
 #ui 镜像
 echo "start run ui module"
 docker run -p 9091:9091  -it -d --net bistoury --ip 172.19.0.4 registry.cn-hangzhou.aliyuncs.com/bistoury/bistoury-ui --zk-address 172.19.0.2:2181 --ui-jdbc-url jdbc:mysql://172.19.0.7:3306/bistoury
